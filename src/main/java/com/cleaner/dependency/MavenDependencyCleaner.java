a00c8e67e402ece9a3bdd3cb79adfa30babd2199
JUNAID B S <junaidkummil73@gmial.com>
2022-10-10 22:26:45 +0530

    Initial commit

---
+++ src/main/java/com/cleaner/dependency/MavenDependencyCleaner.java
@@ -0,0 +1,178 @@
+package com.cleaner.dependency;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.json.JSONException;
+
+/**
+ * @author JUNAID B S
+ *
+ */
+public class MavenDependencyCleaner
+{
+
+	static String line = "", str = "";
+	static String projectPath = "C:/Users/JUNAID B S/Downloads/dependency_cleaner (1)/dependency_cleaner/";
+	static String jarEndString = "Tomcat started on port(s):";
+
+	public static void main(String[] args)
+			throws JSONException, MavenInvocationException, java.io.IOException, XmlPullParserException
+	{
+		if (args.length < 1)
+		{
+			System.out.println("invalid number of arguments project path (folder contains pom)  required");
+			System.exit(0);
+
+		}
+		if (args.length == 2)
+		{
+			jarEndString = args[1];
+		}
+
+		// one input and other output
+
+		try
+		{
+			File src = new File(projectPath + "pom.xml");
+			File dest = new File(projectPath + "backup_pom.xml");
+			Files.copy(src.toPath(), dest.toPath());
+		} catch (Exception e)
+		{
+
+		}
+
+		// using copy(InputStream,Path Target); method
+
+		MavenXpp3Reader reader = new MavenXpp3Reader();
+		Model model = reader.read(new FileReader(projectPath + "pom.xml"));
+		List<Dependency> dependencies = model.getDependencies();
+		List<Dependency> tempDependencies = new ArrayList<>(dependencies);
+		String jarName = model.getArtifactId() + "-" + model.getVersion() + ".jar";
+
+		for (Dependency dependency : tempDependencies)
+
+		{
+			System.out.println(
+					"\n ------------------------Dependency: " + dependency.getArtifactId() + "----------------------");
+			model.removeDependency(dependency);
+			savepom(model);
+			try
+			{
+				if (!(buildJar() && checkJarRunnable(jarName)))
+				{
+
+					model.addDependency(dependency);
+					savepom(model);
+					System.out.println("dependency required");
+				} else
+				{
+					System.out.println("dependency deleted");
+				}
+
+			} catch (MavenInvocationException e)
+			{
+				e.printStackTrace();
+				model.addDependency(dependency);
+				savepom(model);
+			}
+		}
+
+		// buildJar();
+		// checkJarRunnable();
+
+	}
+
+	private static void savepom(Model model)
+	{
+		try
+		{
+			new MavenXpp3Writer().write(new FileOutputStream(new File(projectPath, "pom.xml")), model);
+		} catch (java.io.IOException e)
+		{
+
+			e.printStackTrace();
+		}
+	}
+
+	private static boolean buildJar() throws MavenInvocationException
+	{
+		InvocationRequest request = new DefaultInvocationRequest();
+		request.setPomFile(new File(projectPath, "pom.xml"));
+
+		request.setGoals(Collections.singletonList("clean install -DskipTests"));
+
+		Invoker invoker = new DefaultInvoker();
+		invoker.setMavenHome(new File("C:/Users/JUNAID B S/Downloads/apache-maven-3.8.6-bin/apache-maven-3.8.6"));
+		InvocationResult result = invoker.execute(request);
+
+		if (result.getExitCode() != 0)
+		{
+			return false;
+		} else
+		{
+			System.out.println("succes");
+			return true;
+		}
+	}
+
+	private static boolean checkJarRunnable(String jarName) throws java.io.IOException
+	{
+
+		ProcessBuilder processBuilder = new ProcessBuilder();
+
+		// Run a command
+		processBuilder.command("java", "-jar", projectPath + "/target/" + jarName);
+
+		try
+		{
+			boolean succesflag = false;
+
+			Process process = processBuilder.start();
+
+			StringBuilder output = new StringBuilder();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			String line;
+			while ((line = reader.readLine()) != null)
+			{
+				output.append(line + "\n");
+				System.out.println(line);
+				if (line.contains(jarEndString))
+				{
+					succesflag = true;
+					process.destroy();
+
+				}
+
+			}
+
+			return succesflag;
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+}
